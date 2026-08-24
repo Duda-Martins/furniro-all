2ce458bcc768329878d9e35dcf1a928192fcabb5
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2026-08-24 16:38:40 -0300

    fix: fixing back-end jwt.

--- furniro-back-end/src/auth/strategies/jwt.strategy.ts
+++ furniro-back-end/src/auth/strategies/jwt.strategy.ts
@@ -2,6 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { loadEnvFile } from 'node:process';
+
+loadEnvFile();
+
+const getJwtSecret = (): string => {
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!jwtSecret) {
+    throw new Error('JWT_SECRET is not configured');
+  }
+
+  return jwtSecret as string;
+};
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -9,9 +22,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: !process.env.JWT_SECRET
-        ? 'your_jwt_secret_key'
-        : process.env.JWT_SECRET,
+      secretOrKey: getJwtSecret() as string,
     });
   }
 
